e7d5ea89b450bc6999871cf27f25c292d17311d0
Niks Graves <niksgraves@gmail.com>
2026-09-12 12:42:04 -0700

    set name in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/your-repository-name/', // 👈 Replace with your exact repository name
+  base: '/aethelclock/', // 👈 Replace with your exact repository name
 })
